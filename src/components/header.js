cf4a2ba4855533d5b009c676d042cde20db59b70
Jatin Partap Rana <>
2023-05-04 22:51:07 -0300

    changes from the team messages

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <nav>
             <div  style={{ textAlign: "center"}} className="logo">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             All the best Adam, We will miss you &hearts;&hearts;
 
             </div>
--- src/data/product_data.js
+++ src/data/product_data.js
@@ -1,45 +1,38 @@
 const product_card = [
     {
         id: 1,
-        product_name: "You Name",
-        description: "What ever you want to say to Adam.",
-        price: "Small Title",
-        thumb: "./images/Jj.png"
+        product_name: "Mateus",
+        description: "Thank you so much for enlighting all of us with your eagerness to learn and explore during this time! Hope your challenging spirit always stays with you.I wish you a lot of success and happiness in this next step (and also that you find a good sushi place in San Francisco) Fly high, man!",
+        price: "All the best",
+        thumb: "./images/Mateus.png"
     },
     {
         id: 2,
-        product_name: "For Example",
-        description: "Check the next one",
-        price: "Title",
-        thumb: "./images/Jj.png"
+        product_name: "Giovanny",
+        description: "Just wanted to say how much we'll miss you around here, but we're also super excited for you to chase your dream of founding your own AI company. It's a big move, but I have no doubt you'll crush it. Thanks for being an awesome team member and bringing so much to the table. Your passion for tech and technical skills are truly impressive and inspiring. Make sure to keep in touch and let us know how it's going. We'll be cheering you on from here!",
+        price: "All the best",
+        thumb: "./images/giovanny.png"
     },
     {
         id: 3,
         product_name: "Jatin",
-        description: "I will miss our talk and thanks for being a great friend",
-        price: "You Rock",
-        thumb: "./images/Jj.png"
+        description: "You have been an invaluable member of our team, bringing your unique talents and insights to every project we worked on together. While I am sad to see you go, I am also excited for you as you embark on this new adventure. I know that you will bring the same level of passion, dedication, and hard work to your new endeavors, and that you will achieve great things. Thank you for being an awesome team member and it was always fun working with you. Will miss you a lot :)",
+        price: "Stay In Touch",
+        thumb: "./images/Jatin.png"
     },
     {
         id: 4,
-        product_name: "For Example",
-        description: "I will miss our talk and thanks for being a great friend",
-        price: "Stay In Touch",
-        thumb: "./images/Jj.png"
+        product_name: "Julia",
+        description: "Adam, I want to say a big, big thank you for all the amazing work you've done here at Article! It was truly a pleasure working with you, and your mastery of various skills, particularly in the realm of trees, has been nothing short of impressive. Your ability to make things happen and come up with creative solutions for complex problems has made our collaboration so enjoyable, and I have no doubt that this will be useful in your next venture. It's clear to me that you have an entrepreneurial spirit, and I couldn't be happier for you as you embark on your once-in-a-lifetime opportunity to join Y-combinator. Congratulations! I am incredibly proud of you and wish you all the best in this exciting new chapter. You will be missed! Thank you for everything",
+        price: "Thank You",
+        thumb: "./images/Julia.png"
     },
     {
         id: 5,
-        product_name: "For Example",
-        description: "I will miss our talk and thanks for being a great friend",
-        price: "You Rock",
-        thumb: "./images/Jj.png"
-    },
-    {
-        id: 6,
-        product_name: "For Example",
-        description: "I will miss our talk and thanks for being a great friend",
-        price: "You Rock",
-        thumb: "./images/Jj.png"
+        product_name: "Ailin",
+        description: "Best of luck on your new adventure, Adam! Forever grateful for your mentorship",
+        price: "Please keep in touch!",
+        thumb: "./images/Ailin.png"
     }
 ]
 export default product_card;
